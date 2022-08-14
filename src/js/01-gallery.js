b22dfa7bcf8805d149bbae6a9c140339d35cd0dc
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-08-14 23:40:51 +0300

    зробив частину 1го завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,26 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const galleryBox = document.querySelector(".gallery");
+
+const creatImgGallery = createGalleryBox(galleryItems);
+
+galleryBox.insertAdjacentHTML("beforeend", creatImgGallery);
+
+function createGalleryBox(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+    `;
+    })
+    .join("");
+}
+console.log(createGalleryBox);
